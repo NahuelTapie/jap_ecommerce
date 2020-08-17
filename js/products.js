51ce22f4a590347460964b4b6afc689f7f645beb
NahuelTapie <68575172+NahuelTapie@users.noreply.github.com>
2020-08-16 21:25:55 -0300

    products

--- js/products.js
+++ js/products.js
@@ -1,6 +1,46 @@
+var productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="prod-group-item prod-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name +`</h4>
+                        <p>` + category.description +`</p><br><br>
+                        <h5>` +category.currency+ " "+category.cost +`</h5>
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` vendidos</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductsList(productsArray);
+        }
+    });
 });
