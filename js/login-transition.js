07294d88f00fc7cee47a0c555fec4705804a4912
NahuelTapie <68575172+NahuelTapie@users.noreply.github.com>
2020-08-28 09:45:38 -0300

    login

---
+++ js/login-transition.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function add() {
+	let input = this.parentNode.parentNode;
+	input.classList.add("focus");
+}
+
+function remove() {
+	let input = this.parentNode.parentNode;
+	if (this.value == "") {
+		input.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", add);
+	input.addEventListener("blur", remove);
+});
--- js/login.js
+++ js/login.js
@@ -1,26 +1,3 @@
-const inputs = document.querySelectorAll(".input");
-
-
-function add() {
-	let input = this.parentNode.parentNode;
-	input.classList.add("focus");
-}
-
-function remove() {
-	let input = this.parentNode.parentNode;
-	if (this.value == "") {
-		input.classList.remove("focus");
-	}
-}
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", add);
-	input.addEventListener("blur", remove);
-});
-
-
-
 addEventListener("DOMContentLoaded", function(e){
 	document.getElementsByClassName("btn").addEventListener("onclick", function(){
 		let email = getElementsByClassName("email");
