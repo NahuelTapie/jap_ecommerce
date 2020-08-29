c634b31996578017978de8b94f4588ae9daf8358
NahuelTapie <68575172+NahuelTapie@users.noreply.github.com>
2020-08-29 20:28:25 -0300

    login

---
+++ js/login-transition.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function add() {
+	let input = this.parentNode.parentNode;
+	input.classList.add("focus");
+}
+
+function remove() {
+	let input = this.parentNode.parentNode;
+	if (this.value == "") {
+		input.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", add);
+	input.addEventListener("blur", remove);
+});
+
